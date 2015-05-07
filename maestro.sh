ce5fa6b30bd31662c2bb4c214b84ed1a8e30608e
Maxime Petazzoni <max@signalfx.com>
2015-05-07 11:26:05 -0700

    Switch to Alpine Linux
    Signed-off-by: Maxime Petazzoni <max@signalfx.com>

--- maestro.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-export DEBIAN_FRONTEND=noninteractive
-
-# Install Maestro for guest utils and extensions.
-apt-get -y install python-pip
-pip install http://github.com/signalfuse/maestro-ng/archive/maestro-${1}.zip
